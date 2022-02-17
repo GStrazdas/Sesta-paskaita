9b8e5765c8e18cb9779f1724d5c43212c6270722
GStrazdas <ged.strazdas@gmail.com>
2022-02-17 22:05:54 +0200

    Main papildymas: klases pridejimas su kintamaisiais

---
+++ PraktineUzduotis/Calculation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PraktineUzduotis
+{
+    internal class Calculation
+    {
+        public double earthArea;
+        public double earthVolume;
+        public double hypotenuse;
+        public double ourSum;
+        public double ourAverage;
+    }
+}
