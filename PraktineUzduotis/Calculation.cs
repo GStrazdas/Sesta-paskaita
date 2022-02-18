372c4a9e8c8f2ef9d68e569ec1fa8d8c89c6ba43
JolitaVer <verseckaitejolita@gmail.com>
2022-02-18 20:57:54 +0200

    Maziau kodo

--- PraktineUzduotis/Calculation.cs
+++ PraktineUzduotis/Calculation.cs
@@ -8,17 +8,17 @@ namespace PraktineUzduotis
 {
     internal class Calculation
     {
+        public double earthArea;
+        public double earthVolume;
         public double hypotenuse;
         public void CalcHypotenuse()
         {
-            var a = 10;
-            var sqrA = a*a;
-            var b = 15;
-            var sqrB = b*b;
-            var sqrC = sqrA + sqrB;
-            var hypotenuse = Math.Sqrt(sqrC);
-            hypotenuse = Math.Round(hypotenuse);
-            Console.WriteLine($"*Hypotenuse: {hypotenuse};");
+            int a = 10;
+            int b = 15;
+            double hypotenuse = Math.Sqrt((a * a) + (b * b));
+            Console.WriteLine($"* Hypotenuse: {Math.Round(hypotenuse)};");
         }
+        public double ourSum;
+        public double ourAverage;
     }
 }
