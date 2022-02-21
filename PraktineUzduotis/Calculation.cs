04f7bbd0f26cf1305f0f6d674bb528abd0ef295c
JolitaVer <verseckaitejolita@gmail.com>
2022-02-21 19:22:36 +0200

    Ištaisiau klaidą įžambinės skaičiavime.

--- PraktineUzduotis/Calculation.cs
+++ PraktineUzduotis/Calculation.cs
@@ -13,8 +13,7 @@ namespace PraktineUzduotis
         {
             int radius = 6371;
             double constant = Math.PI;
-            earthArea = Math.Round((radius * radius) * (4 * constant), 2);
-            Console.WriteLine($"eartArea: { earthArea}");
+            earthArea = Math.Round((radius * radius) * (4 * constant), 0);
         }
         public double earthVolume;
         public double hypotenuse;
@@ -22,12 +21,11 @@ namespace PraktineUzduotis
         {
             int a = 10;
             int b = 15;
-            double hypotenuse = Math.Sqrt((a * a) + (b * b));
-            hypotenuse = Math.Round(hypotenuse);
+            hypotenuse = Math.Sqrt((a * a) + (b * b));
+            hypotenuse = Math.Round(hypotenuse, 0);
          }
         private void EarthVolume()
         {
-            /*Skaiciavimas*/
             const double earthRadius = 6371;
             earthVolume = Math.Round(Math.PI * Math.Pow(earthRadius, 3) * 4 / 3, 0);
         }
